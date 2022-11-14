547300227b7a6890c587a28d9cc0094f52f87bf4
Serhii Semeniuk <sergsemeniuk@gmail.com>
2022-11-14 15:38:08 +0200

    createReducer() different syntax

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -12,26 +12,44 @@ const initialState = {
     heroesLoadingStatus: 'idle'
 }
 
-const heroes = createReducer(initialState, builder => {
-    builder
-       .addCase(heroesFetching, state => {
-           state.heroesLoadingStatus = 'loading'
-       })
-       .addCase(heroesFetched, (state, action) => {
+const heroes = createReducer(initialState,{
+       [heroesFetching]: state => { state.heroesLoadingStatus = 'loading' },
+       [heroesFetched]: (state, action) => {
            state.heroesLoadingStatus = 'idle'
            state.heroes = action.payload
-       })
-       .addCase(heroesFetchingError, state => {
-           state.heroesLoadingStatus = 'error'
-       })
-       .addCase(heroCreated, (state, action) => {
+       },
+       [heroesFetchingError]: state => { state.heroesLoadingStatus = 'error' },
+       [heroCreated]: (state, action) => {
            state.heroes.push(action.payload)
-       })
-       .addCase(heroDeleted, (state, action) => {
-           state.heroes.filter(item => item.id !== action.payload)
-       })
-       .addDefaultCase(() => {})
-})
+       },
+       [heroDeleted]: (state, action) => {
+           state.heroes = state.heroes.filter(item => item.id !== action.payload)
+       }
+   },
+   [],
+   state => state
+)
+
+// const heroes = createReducer(initialState, builder => {
+//     builder
+//        .addCase(heroesFetching, state => {
+//            state.heroesLoadingStatus = 'loading'
+//        })
+//        .addCase(heroesFetched, (state, action) => {
+//            state.heroesLoadingStatus = 'idle'
+//            state.heroes = action.payload
+//        })
+//        .addCase(heroesFetchingError, state => {
+//            state.heroesLoadingStatus = 'error'
+//        })
+//        .addCase(heroCreated, (state, action) => {
+//            state.heroes.push(action.payload)
+//        })
+//        .addCase(heroDeleted, (state, action) => {
+//            state.heroes.filter(item => item.id !== action.payload)
+//        })
+//        .addDefaultCase(() => {})
+// })
 
 // const heroes = (state = initialState, action) => {
 //     switch (action.type) {
